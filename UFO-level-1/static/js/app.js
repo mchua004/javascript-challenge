d88a0b163d7b5876c2bef9fe384b9b82c6b4aff7
unknown <mchua004@gmail.com>
2021-04-07 23:13:49 -0400

    Append a cell in table row for each value in each object

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,6 +5,17 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 data.forEach(function(ufoSighting) {
+    // console.log each object in UFO dataset
     console.log(ufoSighting);
+    
+    // append one table row 'tr' for each UFO object
     var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        // console.log each value for each array object
+        console.log(value);
+
+        // append a cell in table row for each value in each object
+        var cell = row.append("td");
+    });
 });
