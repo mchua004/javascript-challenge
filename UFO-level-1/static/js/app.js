5894769d1159e79b8c879f80324fa261cdcef09b
unknown <mchua004@gmail.com>
2021-04-07 22:55:57 -0400

    Reference table body in index.html & console.log each object in data array

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,9 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+});
